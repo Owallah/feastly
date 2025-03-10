60e1e0da7eb3ef9739e5f022cf63b52048ba2362
Owallah <mikesonowallah@gmail.com>
2025-03-11 01:14:02 +0300

    added fetching a random meal

--- src/context/useMealsStore.ts
+++ src/context/useMealsStore.ts
@@ -5,17 +5,20 @@ import axios from "axios";
 interface MealState {
   meals: Meal[];
   categories: Category[];
+  randomMeal: Meal[]
   loading: boolean;
   error: string | null;
   selectedCategory: string | null;
   fetchMeals: (query: string) => Promise<void>;
   fetchCategories: () => Promise<void>;
+  fetchRandomMeal: () => Promise<void>;
   setSelectedCategory: (category: string | null) => void;
 }
 
 const useMealsStore = create<MealState>((set) => ({
   meals: [],
   categories: [],
+  randomMeal: [],
   loading: false,
   error: null,
   selectedCategory: null,
@@ -27,6 +30,21 @@ const useMealsStore = create<MealState>((set) => ({
         `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
       );
       set({ meals: response.data.meals || [], loading: false });
+    } catch (error) {
+      set({
+        error: error instanceof Error ? error.message : "Error fetching meals",
+        loading: false,
+      });
+    }
+  },
+
+  fetchRandomMeal: async () => {
+    set({ loading: true, error: null });
+    try {
+      const response = await axios.get<MealResponse>(
+        `https://www.themealdb.com/api/json/v1/1/random.php`
+      );
+      set({ randomMeal: response.data.meals || [], loading: false });
     } catch (error) {
       set({
         error: error instanceof Error ? error.message : "Error fetching meals",
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,18 +3,41 @@ import "../assets/styles/HomePage.css";
 import MealList from "../components/MealList";
 import useMealsStore from "../context/useMealsStore";
 import CategoryFilter from "../components/CategoryFilter";
+import MealCard from "../components/MealCard";
 
 
 const HomePage = () => {
-    const { fetchCategories } = useMealsStore();
+    const { fetchCategories, fetchRandomMeal, randomMeal } = useMealsStore();
 
     useEffect(() => {
       fetchCategories();
     }, [fetchCategories])
+
+    useEffect(() => {
+        fetchRandomMeal();
+      }, [fetchRandomMeal])
+
+    // useEffect(() => {
+    //     const timer = setTimeout(() => {
+    //       fetchRandomMeal();
+    //     }, 10000); // 10 seconds in milliseconds
+      
+    //     // Cleanup the timer if the component unmounts
+    //     return () => clearTimeout(timer);
+    //   }, [fetchRandomMeal]);
   
   return <section className="home container">
     <h1>HomePage</h1>
     <CategoryFilter />
+    
+    {/* // random meal goes here */}
+    {randomMeal.length > 0 && (
+        <div className="random-meal">
+          <h2>Random Meal</h2>
+          <MealCard {...randomMeal[0]} />
+        </div>
+      )}
+
     <MealList query="" />
   </section>;
 };
